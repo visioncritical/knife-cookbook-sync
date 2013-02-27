2d6a679b2526c7d1bf438395ac5013e70cd68096
Erik Hollensbe <erik+github@hollensbe.org>
2013-02-26 17:23:22 -0800

    handle exit statuses from cookbook sync a little better.

--- lib/chef-workflow/tasks/chef/cookbooks/sync.rb
+++ lib/chef-workflow/tasks/chef/cookbooks/sync.rb
@@ -7,7 +7,7 @@ namespace :chef do
     task :sync => [ "bootstrap:knife" ] do
       Rake::Task["chef:cookbooks:resolve"].invoke rescue nil
       result = knife %W[cookbook sync -a]
-      fail if result != 0
+      fail unless [0, 5].include?(result)
     end
   end
 end
