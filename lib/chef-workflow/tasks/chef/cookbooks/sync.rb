659e299d2f2b47f64dc5507bdc79114605e2f3b4
Erik Hollensbe <erik+github@hollensbe.org>
2013-03-12 01:49:39 -0700

    allow exceptions for resolver task to bubble

--- lib/chef-workflow/tasks/chef/cookbooks/sync.rb
+++ lib/chef-workflow/tasks/chef/cookbooks/sync.rb
@@ -5,7 +5,9 @@ namespace :chef do
   namespace :cookbooks do
     desc "Upload your cookbooks to the chef server"
     task :sync => [ "bootstrap:knife" ] do
-      Rake::Task["chef:cookbooks:resolve"].invoke rescue nil
+      resolve_task = Rake::Task["chef:cookbooks:resolve"] rescue nil
+      resolve_task.invoke if resolve_task
+
       result = knife %W[cookbook sync -a]
       fail unless [0, 5].include?(result)
     end
